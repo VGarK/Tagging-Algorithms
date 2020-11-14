47ae21298da45d0ae9fa425ccf30f9e0c3a47b27
VGarK <vgarcab@gmail.com>
2020-11-14 14:08:39 +0100

    Added algorithm

---
+++ KMeans.py
@@ -0,0 +1,228 @@
+
+"""
+
+@author: ramon, bojana
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as axes3d
+from sklearn.decomposition import PCA
+
+
+def NIUs():
+    return 1458626, 1458383, 1111113
+
+def distance(X,C):
+    """@brief   Calculates the distance between each pixel and each centroid
+
+    @param  X  numpy array PxD 1st set of data points (usually data points)
+    @param  C  numpy array KxD 2nd set of data points (usually cluster centroids points)
+
+    @return dist: PxK numpy array position ij is the distance between the
+    	i-th point of the first set an the j-th point of the second set
+    """
+    return np.array([[np.linalg.norm(c-p) for c in C] for p in X])
+
+class KMeans():
+
+    def __init__(self, X, K, options=None):
+        """@brief   Constructor of KMeans class
+
+        @param  X   LIST    input data
+        @param  K   INT     number of centroids
+        @param  options DICT dctionary with options
+        """
+
+        self._init_X(X)                                    # LIST data coordinates
+        self._init_options(options)                        # DICT options
+        self._init_rest(K)                                 # Initializes de rest of the object
+
+#############################################################
+##  THIS FUNCTION CAN BE MODIFIED FROM THIS POINT, if needed
+#############################################################
+
+
+    def _init_X(self, X):
+        """@brief Initialization of all pixels
+
+        @param  X   LIST    list of all pixel values. Usually it will be a numpy
+                            array containing an image NxMx3
+
+        sets X an as an array of data in vector form (PxD  where P=N*M and D=3 in the above example)
+        """
+        self.X = X.reshape(np.prod(X.shape[:-1]), X.shape[-1])
+
+
+    def _init_options(self, options):
+        """@brief Initialization of options in case some fields are left undefined
+
+        @param  options DICT dctionary with options
+
+			sets de options parameters
+        """
+        if options == None:
+            options = {}
+        if not 'km_init' in options:
+            options['km_init'] = 'first'
+        if not 'verbose' in options:
+            options['verbose'] = False
+        if not 'tolerance' in options:
+            options['tolerance'] = 0
+        if not 'max_iter' in options:
+            options['max_iter'] = np.inf
+        if not 'fitting' in options:
+            options['fitting'] = 'Fisher'
+
+        self.options = options
+
+#############################################################
+##  THIS FUNCTION CAN BE MODIFIED FROM THIS POINT, if needed
+#############################################################
+
+
+    def _init_rest(self, K):
+        """@brief   Initialization of the remainig data in the class.
+
+        @param  options DICT dctionary with options
+        """
+        self.K = K                                             # INT number of clusters
+        if self.K>0:
+            self._init_centroids()                             # LIST centroids coordinates
+            self.old_centroids = np.empty_like(self.centroids) # LIST coordinates of centroids from previous iteration
+            self.clusters = np.zeros(len(self.X))              # LIST list that assignes each element of X into a cluster
+            self._cluster_points()                             # sets the first cluster assignation
+        self.num_iter = 0                                      # INT current iteration
+
+#############################################################
+##  THIS FUNCTION CAN BE MODIFIED FROM THIS POINT, if needed
+#############################################################
+
+
+    def _init_centroids(self):
+        """@brief Initialization of centroids
+        depends on self.options['km_init']
+        """
+        if self.options['km_init'].lower() == 'first':
+                self.centroids = np.array([list(b) for b in list(dict.fromkeys([tuple(row) for row in self.X]))][:self.K], dtype=np.float64)
+        elif self.options['km_init'].lower() == 'random':
+	        self.centroids = self.X[np.random.choice(self.X.shape[0], self.K, replace=False), :]
+        if self.options['km_init'].lower() == 'spaced':
+                self.centroids=np.zeros((self.K, self.X.shape[1]))
+                for k in range(self.K): self.centroids[k, :] = k*255/(self.K-1)
+
+    def _cluster_points(self):
+        """@brief   Calculates the closest centroid of all points in X
+        """
+        #self.clusters = np.random.randint(self.K,size=self.clusters.shape)
+        #print(distance(self.X, self.centroids)[:100][-6:])
+        #self.clusters = np.array([[np.linalg.norm(c-p) for c in self.centroids] for p in self.X]).argmin(axis=1)
+        #self.clusters = np.array([[np.linalg.norm(c-p) for c in self.centroids] for p in self.X]).argmin(axis=1)
+        self.clusters = distance(self.X, self.centroids).argmin(axis=1)
+
+
+    def _get_centroids(self):
+        """@brief   Calculates coordinates of centroids based on the coordinates
+                    of all the points assigned to the centroid
+        """
+        self.old_centroids = np.copy(self.centroids)
+        self.centroids = np.array([np.mean(self.X[np.where(self.clusters==c)], axis=0) for c in range(len(self.centroids))])
+
+
+    def _converges(self):
+        """@brief   Checks if there is a difference between current and old centroids
+        """
+        return not np.any(np.linalg.norm(self.centroids-self.old_centroids, axis=1)>self.options['tolerance'])
+
+    def _iterate(self, show_first_time=True):
+        """@brief   One iteration of K-Means algorithm. This method should
+                    reassigne all the points from X to their closest centroids
+                    and based on that, calculate the new position of centroids.
+        """
+        self.num_iter += 1
+        self._cluster_points()
+        self._get_centroids()
+        if self.options['verbose']:
+            self.plot(show_first_time)
+
+
+    def run(self):
+        """@brief   Runs K-Means algorithm until it converges or until the number
+                    of iterations is smaller than the maximum number of iterations.=
+        """
+        if self.K==0:
+            self.bestK()
+            return
+
+        self._iterate(True)
+        if self.options['max_iter'] > self.num_iter:
+            while not self._converges():
+                self._iterate(False)
+
+
+    def bestK(self):
+        """@brief   Runs K-Means multiple times to find the best K for the current
+                    data given the 'fitting' method. In cas of Fisher elbow method
+                    is recommended.
+
+                    at the end, self.centroids and self.clusters contains the
+                    information for the best K. NO need to rerun KMeans.
+           @return B is the best K found.
+        """
+        K = 1
+        self._init_rest(K)
+        self.run()
+        fit = self.fitting()
+        ant = fit
+        ant2 = ant
+        while (ant-fit)*2 < (ant2 - ant):
+            self._init_rest(K)
+            self.run()
+            ant2 = ant
+            ant = fit
+            fit = self.fitting()
+            K += 1
+        return K
+
+    def fitting(self):
+        """@brief  return a value describing how well the current kmeans fits the data
+        """
+        if self.options['fitting'].lower() == 'fisher':
+            intra = np.array([[np.linalg.norm(self.centroids[i]-p)/(np.sum(self.clusters==i)) for i in range(len(self.centroids))] for p in self.X]).sum()/self.K
+            center = np.mean(self.X,axis=0)
+            inter = np.array([np.linalg.norm(c-center) for c in self.centroids]).sum()/self.K
+            return intra/inter
+        elif self.options['fitting'].lower() == 'silhouette':
+            return np.random.rand(1)
+
+
+    def plot(self, first_time=True):
+        """@brief   Plots the results
+        """
+
+        #markersshape = 'ov^<>1234sp*hH+xDd'
+        markerscolor = 'bgrcmybgrcmybgrcmyk'
+        if first_time:
+            plt.gcf().add_subplot(111, projection='3d')
+            plt.ion()
+            plt.show()
+
+        if self.X.shape[1]>3:
+            if not hasattr(self, 'pca'):
+                self.pca = PCA(n_components=3)
+                self.pca.fit(self.X)
+            Xt = self.pca.transform(self.X)
+            Ct = self.pca.transform(self.centroids)
+        else:
+            Xt=self.X
+            Ct=self.centroids
+
+        for k in range(self.K):
+            plt.gca().plot(Xt[self.clusters==k,0], Xt[self.clusters==k,1], Xt[self.clusters==k,2], '.'+markerscolor[k])
+            plt.gca().plot(Ct[k,0:1], Ct[k,1:2], Ct[k,2:3], 'o'+'k',markersize=12)
+
+        if first_time:
+            plt.xlabel('dim 1')
+            plt.ylabel('dim 2')
+            plt.gca().set_zlabel('dim 3')
+        plt.draw()
+        plt.pause(0.01)
---
+++ Labels.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: ramon, bojana
+"""
+import re
+import numpy as np
+import ColorNaming as cn
+from skimage import color
+import KMeans as km
+
+def NIUs():
+    return 1458383, 1458626, 3333333
+
+def loadGT(fileName):
+    """@brief   Loads the file with groundtruth content
+
+    @param  fileName  STRING    name of the file with groundtruth
+
+    @return groundTruth LIST    list of tuples of ground truth data
+                                (Name, [list-of-labels])
+    """
+
+    groundTruth = []
+    fd = open(fileName, 'r')
+    for line in fd:
+        splitLine = line.split(' ')[:-1]
+        labels = [''.join(sorted(filter(None,re.split('([A-Z][^A-Z]*)',l)))) for l in splitLine[1:]]
+        groundTruth.append( (splitLine[0], labels) )
+
+    return groundTruth
+
+
+def evaluate(description, GT, options):
+    """@brief   EVALUATION FUNCTION
+    @param description LIST of color name lists: contain one lsit of color labels for every images tested
+    @param GT LIST images to test and the real color names (see  loadGT)
+    @options DICT  contains options to control metric, ...
+    @return mean_score,scores mean_score FLOAT is the mean of the scores of each image
+                              scores     LIST contain the similiraty between the ground truth list of color names and the obtained
+    """
+    scores = [0] * len(description)
+    for i in range (0, len(description)):
+        scores[i] = similarityMetric(description[i], GT[i], options)
+    return sum(scores)/len(description), scores
+
+
+
+def similarityMetric(Est, GT, options):
+    """@brief   SIMILARITY METRIC
+    @param Est LIST  list of color names estimated from the image ['red','green',..]
+    @param GT LIST list of color names from the ground truth
+    @param options DICT  contains options to control metric, ...
+    @return S float similarity between label LISTs
+    """
+
+    if options == None:
+        options = {}
+    if not 'metric' in options:
+        options['metric'] = 'basic'
+
+#########################################################
+##  YOU MUST REMOVE THE REST OF THE CODE OF THIS FUNCTION
+##  AND CHANGE FOR YOUR OWN CODE
+#########################################################
+    comptador = 0
+    if options['metric'].lower() == 'basic':
+        for i in Est:
+            if i in GT[1]:
+                comptador = comptador + 1
+        return comptador / len(Est)
+
+    else:
+        return 0
+
+def getLabels(kmeans, options):
+    """@brief   Labels all centroids of kmeans object to their color names
+
+    @param  kmeans  KMeans      object of the class KMeans
+    @param  options DICTIONARY  options necessary for labeling
+
+    @return colors  LIST    colors labels of centroids of kmeans object
+    @return ind     LIST    indexes of centroids with the same color label
+    """
+    colors = []
+    unics=[]
+    for i, c in enumerate(kmeans.centroids):
+        sc=np.flip(np.argsort(c))
+        color=""
+        if c[sc[0]]>options['single_thr']:
+           color=cn.colors[sc[0]]
+        else:
+           colorSort=sorted([cn.colors[sc[0]], cn.colors[sc[1]]])
+           color=colorSort[0]+colorSort[1]
+        if color in colors:
+            unics[colors.index(color)].append(i)
+        else:
+            colors.append(color)
+            unics.append([i])
+
+    return colors, unics
+
+
+def processImage(im, options):
+    """@brief   Finds the colors present on the input image
+
+    @param  im      LIST    input image
+    @param  options DICTIONARY  dictionary with options
+
+    @return colors  LIST    colors of centroids of kmeans object
+    @return indexes LIST    indexes of centroids with the same label
+    @return kmeans  KMeans  object of the class KMeans
+    """
+
+#########################################################
+##  YOU MUST ADAPT THE CODE IN THIS FUNCTIONS TO:
+##  1- CHANGE THE IMAGE TO THE CORRESPONDING COLOR SPACE FOR KMEANS
+##  2- APPLY KMEANS ACCORDING TO 'OPTIONS' PARAMETER
+##  3- GET THE NAME LABELS DETECTED ON THE 11 DIMENSIONAL SPACE
+#########################################################
+
+##  1- CHANGE THE IMAGE TO THE CORRESPONDING COLOR SPACE FOR KMEANS
+    if options['colorspace'].lower() == 'ColorNaming'.lower():
+        im = cn.ImColorNamingTSELabDescriptor(im)
+    elif options['colorspace'].lower() == 'RGB'.lower():
+        pass
+    elif options['colorspace'].lower() == 'Lab'.lower():
+        im = color.rgb2lab(im)
+    elif options['colorspace'].lower() == 'HED'.lower():
+        im = color.rgb2hed(im)
+    elif options['colorspace'].lower() == 'HSV'.lower():
+        im = color.rgb2hsv(im)
+    '''
+    elif options['colorspace'].lower() == 'opponent'.lower():
+        im = color.rgb2lab(im)
+    elif options['colorspace'].lower() == 'HSL'.lower():
+        im = color.rgb2(im)
+    elif options['colorspace'].lower() == 'Lab'.lower():
+        im = color.rgb2lab(im)
+    '''
+
+
+##  2- APPLY KMEANS ACCORDING TO 'OPTIONS' PARAMETER
+    if options['K']<2: # find the bes K
+        kmeans = km.KMeans(im, 0, options)
+        kmeans.bestK()
+    else:
+        kmeans = km.KMeans(im, options['K'], options)
+        kmeans.run()
+
+##  3- GET THE NAME LABELS DETECTED ON THE 11 DIMENSIONAL SPACE
+    if options['colorspace'].lower() == 'Lab'.lower():
+        kmeans.centroids = cn.ImColorNamingTSELabDescriptor((color.lab2rgb(kmeans.centroids.reshape(1,len(kmeans.centroids),3))*255).reshape(len(kmeans.centroids),3))
+    elif options['colorspace'].lower() == 'HED'.lower():
+        kmeans.centroids = cn.ImColorNamingTSELabDescriptor(color.hed2rgb(kmeans.centroids.reshape(1,len(kmeans.centroids),3)).reshape(len(kmeans.centroids),3))
+    elif options['colorspace'].lower() == 'HSV'.lower():
+        kmeans.centroids = cn.ImColorNamingTSELabDescriptor((color.hsv2rgb(kmeans.centroids.reshape(1,len(kmeans.centroids),3))*255).reshape(len(kmeans.centroids),3))
+    elif options['colorspace'].lower() == 'RGB'.lower():
+        kmeans.centroids = cn.ImColorNamingTSELabDescriptor(kmeans.centroids)
+
+#########################################################
+##  THE FOLLOWING 2 END LINES SHOULD BE KEPT UNMODIFIED
+#########################################################
+    colors, which = getLabels(kmeans, options)
+    return colors, which, kmeans
